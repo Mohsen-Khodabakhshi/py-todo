c92c0e41b151f478ef4fee6208160d09a1ae40d3
Mohsen Khodabakhshi <mim.khodabakhshi@gmail.com>
2024-07-15 18:01:18 +0330

    feat: validate password

--- apps/auth/schemas.py
+++ apps/auth/schemas.py
@@ -1,12 +1,26 @@
 from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
+from fastapi import HTTPException, status
 
 
-class UserRegisterIn(BaseModel):
+class PasswordModel(BaseModel):
+    password: str
+
+    @field_validator('password')
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        if len(value) < 8:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Password must be at least 8 characters long.",
+            )
+        return value
+
+
+class UserRegisterIn(PasswordModel):
     name: str
     username: str
     email: EmailStr
-    password: str
 
 
 class UserRegisterOut(BaseModel):
@@ -24,7 +38,7 @@ class UserProfileOut(BaseModel):
     last_login: datetime | None = None
 
 
-class UserLoginIn(BaseModel):
+class UserLoginIn(PasswordModel):
     username: str
     password: str
 
